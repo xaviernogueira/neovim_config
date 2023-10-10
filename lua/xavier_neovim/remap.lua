b45a9e75d09ce7d4e33b30d56ee26c2a1edca36c
xaviernogueira <xavier.rojas.nogueira@gmail.com>
2023-10-10 15:29:51 -0400

    Update remap.lua

--- lua/xavier_neovim/remap.lua
+++ lua/xavier_neovim/remap.lua
@@ -7,3 +7,5 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- save with cnrl+s
 vim.keymap.set("n", "<C-s>", ":w<CR>")
 
+-- make sure Esc is not remapped
+vim.keymap.set("i", "<Esc>", "<Esc>", { noremap = true })
