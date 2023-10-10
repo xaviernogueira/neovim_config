a07ee38ffedce3591333c5d5adf4c7601a32c010
xaviernogueira <xavier.rojas.nogueira@gmail.com>
2023-10-10 15:31:03 -0400

    Update venv.lua

--- after/plugin/venv.lua
+++ after/plugin/venv.lua
@@ -1,3 +1,8 @@
-require("venv-selector").setup()
+require("venv-selector").setup(
+    { 
+        anaconda_envs_path = "C:/Users/xrnogueira/Miniconda3/envs", 
+        anaconda = { python_parent_dir = '' },
+    }
+)
 
 vim.keymap.set("n", "<leader>vs", ":VenvSelect<CR>")
