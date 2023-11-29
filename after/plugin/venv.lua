422849b1c2f99770680fe01897951272969b924e
xaviernogueira <xavier.rojas.nogueira@gmail.com>
2023-11-29 15:21:26 -0500

    Update venv.lua

--- after/plugin/venv.lua
+++ after/plugin/venv.lua
@@ -2,6 +2,7 @@ require("venv-selector").setup(
     { 
         anaconda_envs_path = "C:/Users/xrnogueira/Miniconda3/envs", 
         anaconda = { python_parent_dir = '' },
+        pipenv_path = "C:/Users/xrnogueira/.virtualenvs"
     }
 )
 
