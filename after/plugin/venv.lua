ad13c030c33a39d8ebec1a0013f31c2b8abee0f4
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2023-12-18 23:31:38 -0500

    Dropping conda for now

--- after/plugin/venv.lua
+++ after/plugin/venv.lua
@@ -1,8 +1,8 @@
-require("venv-selector").setup(
-    { 
-        anaconda_envs_path = "~/miniconda3/envs", 
-        anaconda = { python_parent_dir = '' },
-    }
-)
+require("venv-selector").setup()
+--    { 
+--        anaconda_envs_path = "~/miniconda3/envs", 
+--        anaconda = { python_parent_dir = '' },
+--    }
+--)
 
 vim.keymap.set("n", "<leader>vs", ":VenvSelect<CR>")
